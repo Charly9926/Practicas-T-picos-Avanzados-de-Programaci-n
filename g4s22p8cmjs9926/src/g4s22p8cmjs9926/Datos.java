fbd277ce47ed6636078d2c602d0332ea51839882
Carlos Mauricio <carloxs9926@gmail.com>
2021-05-12 06:06:44 -0500

    Agregue la practica 7 y 8

---
+++ g4s22p8cmjs9926/src/g4s22p8cmjs9926/Datos.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s22p8cmjs9926;
+
+/**
+ *
+ * @author CMJ
+ */
+public class Datos {
+    String Nombre;
+    String Edad;
+    String Correo;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    
+    
+}
